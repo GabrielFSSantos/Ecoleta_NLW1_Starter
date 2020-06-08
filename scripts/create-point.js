52ca10917ad90f52ff88299e481ce3d5009b1a9e
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-06-08 01:02:38 -0300

    add create-point page

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,4 +1,6 @@
 
+// Dados da Entidade
+
 function populateStates() {
     const stateSelect = document.querySelector("select[name=state]");
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
@@ -17,21 +19,72 @@ function getCities(event) {
     const stateInput = document.querySelector("input[name=state]");
 
     stateInput.value = event.target.options[event.target.selectedIndex].text;
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+    citySelect.disabled = true;
 
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/municipios`)
         .then(res => res.json())
         .then(cities => {
+
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
             citySelect.disabled = false;
         })
 }
 
+document.querySelector("select[name=state]").addEventListener("change", getCities);
 
-document
-    .querySelector("select[name=state]")
-    .addEventListener("change", getCities);
 
-//https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios
+
+// Ítens de Coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+let selectedItems = [];
+const collectedItems = document.querySelector("input[name=items]");
+
+function handleSelectedItem(event) {
+    //Adicionar ou remover class com JS
+    const itemLi = event.target;
+    itemLi.classList.toggle("selected");
+
+    /* Editado: Outra forma de realizar a função
+    // Adicionar ou remover itens selecionados dentro de vetor
+    if(itemLi.classList.value === "selected"){
+        selectedItems.push(itemLi.dataset.id);
+    }
+    else{
+        selectedItems.splice(selectedItems.indexOf(itemLi.dataset.id), 1);
+    }
+    */
+
+    const itemID = itemLi.dataset.id;
+
+    //Verificar se existe items selecionados e Pegar items selecionados
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemID // isso será true ou false
+        return itemFound;
+    });
+
+    //Se já esta selecionado
+    if (alreadySelected >= 0) {
+        //tirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemID;
+            return itemIsDifferent;
+        })
+        selectedItems = filteredItems;
+    } else {
+        //Se não estiver selecionado, adiciona na seleção
+        selectedItems.push(itemID);
+    }
+    
+    //Adicionar ao Input items selecionados
+    collectedItems.value = selectedItems;
+}
 
